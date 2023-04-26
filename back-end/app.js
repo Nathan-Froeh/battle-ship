c667356e296af5d530fa0eb0a680ab93e32f385e
Nathan <nathan.froeh@gmail.com>
2023-04-26 11:16:59 -0600

    npm i

--- back-end/app.js
+++ back-end/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const port = 8080;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,6 +22,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
